1d2b2fe7463b047ca37468507638ac4e66354843
Jonathan Krisna <65582909+abcdgick@users.noreply.github.com>
2024-03-08 13:35:30 +0700

    Added footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,22 @@
             <main>
                 {{ $slot }}
             </main>
+            <footer class="bg-white fixed bottom-0 left-0 right-0 rounded-lg shadow m-4 dark:bg-gray-800">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                  <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/abcdgick" class="hover:underline">Jonathan Krisna</a>. All Rights Reserved.
+                </span>
+                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="mailto:jonathankrisna@protonmail.com" class="hover:underline me-4 md:me-6">Contact Me</a>
+                    </li>
+                </ul>
+                </div>
+            </footer>
         </div>
+
+
+
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
 </html>
